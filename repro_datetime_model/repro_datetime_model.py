baebc7700094f6d8bfc51da83d66b384f7ddd92b
Masen Furer <m_github@0x26.net>
2024-02-21 15:25:38 -0800

    Update for reflex 0.4.1

--- repro_datetime_model/repro_datetime_model.py
+++ repro_datetime_model/repro_datetime_model.py
@@ -6,9 +6,7 @@ from sqlmodel import Field, DateTime, Column, func
 
 class Entry(rx.Model, table=True):
     ts: datetime.datetime = Field(
-        datetime.datetime.now(datetime.timezone.utc),
         sa_column=Column(DateTime(timezone=True), server_default=func.now()),
-        nullable=False,
     )
     text: str = Field(nullable=False)
 
@@ -29,42 +27,70 @@ class State(rx.State):
             session.add(entry)
             session.commit()
             session.refresh(entry)
-        # shove it into the list of entries
-        self.entries.insert(0, entry)
-        return [rx.set_value(field, "") for field in form_data]
+        return [rx.set_value(field, "") for field in form_data] + [rx.redirect("/")]
 
     def load_entries(self):
         """Load entries from the database."""
         with rx.session() as session:
             self.entries = session.exec(Entry.select.order_by(Entry.ts.desc())).all()
 
+    def on_load(self):
+        self.load_entries()
 
+
+def submission_form() -> rx.Component:
+    return rx.form(
+        rx.vstack(
+            rx.heading(rx.hstack("Shout Your Thoughts Into the Void", rx.icon("megaphone"), align="center"), size="2"),
+            rx.input.root(
+                rx.input(placeholder="Enter text here...", id="text"),
+                width="100%",
+            ),
+            rx.hstack(
+                rx.button("Reload", rx.icon("refresh-cw", size=20), on_click=State.load_entries, type="button", color_scheme="gray"),
+                rx.spacer(),
+                rx.button("Post", rx.icon("send", size=20)),
+                width="100%"
+            ),
+            gap="1em",
+        ),
+        on_submit=State.handle_submit,
+    )
+
+def entry(e: Entry) -> rx.Component:
+    return rx.card(
+        rx.vstack(
+            rx.text(e.ts, font_size="0.75em"),
+            rx.text(e.text),
+        ),
+        width="100%",
+    )
+    
+
+@rx.page(on_load=State.on_load)
 def index() -> rx.Component:
     return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.form(
-                rx.input(placeholder="Enter text here...", id="text"),
-                rx.button("Submit", type_="submit"),
-                on_submit=State.handle_submit,
+        rx.color_mode.button(rx.color_mode.icon(), float="right"),
+        rx.center(
+            rx.vstack(
+                rx.card(
+                    submission_form(),
+                    width="100%",
+                ),
+                rx.vstack(
+                    rx.foreach(
+                        State.entries,
+                        entry,
+                    ),
+                    gap="2em",
+                    margin_y="2em",
+                    width="100%",
+                ),
+                width=["100vw", "75vw", "75vw", "50vw", "50vw"],
             ),
-            rx.button("Reload 🔁", on_click=State.load_entries),
-            rx.list(
-                rx.foreach(
-                    State.entries,
-                    lambda e: rx.vstack(
-                        rx.text(e.ts, font_size="0.75em"),
-                        rx.text(e.text),
-                        align_items="start",
-                        margin_bottom="2em",
-                    )
-                )
-            ),
+            width="100%",
         ),
     )
 
 
-# Add state and page to the app.
 app = rx.App()
-app.add_page(index)
-app.compile()
--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,3 @@
 import reflex as rx
 
-class ReprodatetimemodelConfig(rx.Config):
-    pass
-
-config = ReprodatetimemodelConfig(
-    app_name="repro_datetime_model",
-)
+config = rx.Config(app_name="repro_datetime_model")
